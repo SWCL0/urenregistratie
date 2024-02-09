d17b50f0fa831f451893b7fd963722a531ca58d5
SWCL0 <stan.w.c@icloud.com>
2024-02-09 10:47:10 +0100

    update urenregistratie.php
    toevoegingen
    -array dagen
    -foreach loop die door de dagen loopt om uren en minuten op te vragen

--- urenregistratie.php
+++ urenregistratie.php
@@ -1,5 +1,28 @@
 <?php
 
+$dag = array("Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag");
+$uren;
+$minuten;
+$werkdagen = 5;
+$langsteWerkdag;
+
+foreach ($dag as $dagen) {
+    echo "Werkdag: $dagen" . PHP_EOL;
+    echo "Aantal uur: ";
+    $uren = readline();
+    echo "Aantal minuten: ";
+    $minuten = readline();
+
+    if (($uren == 0) && ($minuten == 0)) {
+        $werkdagen--;
+    }
+}
+
+echo "Aantal werkdagen: " . $werkdagen . PHP_EOL;
+echo "Totale werktijd: $uren uur en $minuten minuten" . PHP_EOL;
+echo "Langste werkdag: $langsteWerkdag";
+
+
 
 
 ?>
