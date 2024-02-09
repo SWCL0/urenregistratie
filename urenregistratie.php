431b102df44bf39dd11166a4d2377f907e6ab5a1
SWCL0 <stan.w.c@icloud.com>
2024-02-09 11:05:21 +0100

    update urenregistratie.php
    toevoeging van commentaar regels
    aanpassingen syntax
    toevoegingen van code voor weergaven en berekeningne

--- urenregistratie.php
+++ urenregistratie.php
@@ -1,28 +1,31 @@
 <?php
 
+// Aanmaak alle arrays en variabelen
 $dag = array("Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag");
-$uren;
-$minuten;
+$uren = 0;
+$minuten = 0;
 $werkdagen = 5;
-$langsteWerkdag;
 
+// Ga door elke dag heen
 foreach ($dag as $dagen) {
     echo "Werkdag: $dagen" . PHP_EOL;
     echo "Aantal uur: ";
-    $uren = readline();
+    $uur = readline();
     echo "Aantal minuten: ";
-    $minuten = readline();
-
-    if (($uren == 0) && ($minuten == 0)) {
+    $minuut = readline();
+ 
+// Check of er die dag is gewerkt
+    if (($uur == 0) && ($minuut == 0)) {
         $werkdagen--;
     }
-}
 
+// Voeg de tijd bij de andere tijd op
+    $uren = $uren + $uur;
+    $minuten = $minuten + $minuut;
+} 
+
+// Weergeef alle info
 echo "Aantal werkdagen: " . $werkdagen . PHP_EOL;
-echo "Totale werktijd: $uren uur en $minuten minuten" . PHP_EOL;
-echo "Langste werkdag: $langsteWerkdag";
-
-
-
+echo "Totale werktijd: " . $uren . " uur en " . $minuten . " minuten" . PHP_EOL;
 
 ?>
